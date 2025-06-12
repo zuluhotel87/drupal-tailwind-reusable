2572bcb5dfcb95df32f26356fad130575186e1e5
Francis Lacerte <matrix__boy@hotmail.com>
2025-06-11 22:10:20 -0400

    chore(core): wip hero banner

--- web/themes/custom/shizen/preprocess/links.php
+++ web/themes/custom/shizen/preprocess/links.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-use Drupal\Core\Form\FormStateInterface;
-
 function shizen_preprocess_links__language_block(array &$variables): void
 {
   $currentLanguage = Drupal::languageManager()->getCurrentLanguage()->getId();
